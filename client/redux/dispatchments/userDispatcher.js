24421b683525400513c1c9a819c83dd23c821522
Roody <rjointer2@gmail.com>
2021-07-20 18:06:03 -0700

    chicken wing break,when back will start on the dispacthment function

---
+++ client/redux/dispatchments/userDispatcher.js
@@ -0,0 +1,8 @@
+
+// all constants for user
+import * as constantTypes from '../constants/userConstants';
+
+// this dispatchment function will take the user's form data and 
+// return a function with a dispatch function as a arg to specfic 
+// how the store will change
+
