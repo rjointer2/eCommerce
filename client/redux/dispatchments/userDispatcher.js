a8bfca90b2a0c4755f9c58da0bb4cd3521200636
Roody <rjointer2@gmail.com>
2021-07-20 19:10:37 -0700

    now we have to make the middlewarein the server to create and return a token a jwt auth the user

--- client/redux/dispatchments/userDispatcher.js
+++ client/redux/dispatchments/userDispatcher.js
@@ -1,8 +1,22 @@
 
 // all constants for user
-import * as constantTypes from '../constants/userConstants';
+import * as constants from '../constants/userConstants';
 
 // this dispatchment function will take the user's form data and 
 // return a function with a dispatch function as a arg to specfic 
 // how the store will change
 
+const getUserDetailsRequest = ( formData ) => async ( dispatch ) => {
+
+    try {
+        // when the switch statement's case requesting the user's data
+        // use the useMutation hook sign in and return the useQuery's data
+        dispatch({ type: constants.GET_USER_DETAILS_REQUEST });
+
+        
+
+    } catch(err) {
+
+    }
+
+}
